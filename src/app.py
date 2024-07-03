f0d676cb9e59f52f16e4b088e1ba4116a446b5df
Lucas Lucyk <lucaslucyk@gmail.com>
2024-07-02 21:02:04 -0300

    add database connection

--- src/app.py
+++ src/app.py
@@ -1,11 +1,24 @@
-from flask import Flask
+from flask import Flask, jsonify
+from .database import DbSession
+from .conf import settings
+
 
 app = Flask(__name__)
 
 
 @app.get("/")
 def index():
-    return "Hello, World!"
+    return {"ping": "pong"}
+
+
+@app.get("/samples")
+def list_samples():
+
+    with DbSession(settings.DB_URL) as session:
+        session.execute("SELECT * FROM sample")
+        samples = session.fetchall()
+
+    return jsonify(samples)
 
 
 if __name__ == "__main__":
